1b596649a69a1c3634a6c1979d3bd43fc31db00b
Sebastian <sczabak@gmail.com>
2025-09-11 12:05:38 +0200

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,13 +3,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCF0n63bbexFKlH0YAdnsxdGC9kOQ34mCE',
-  authDomain: 'anoopole-main.firebaseapp.com',
-  projectId: 'anoopole-main',
-  storageBucket: 'anoopole-main.firebasestorage.app',
-  messagingSenderId: '172657938536',
-  appId: '1:172657938536:web:ac46d125ef217b1685ebdd',
-  measurementId: 'G-RVWQXD4FB1',
+
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
